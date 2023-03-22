5fcbaf1ad5f80896234ef9aabee18b75b08068be
fmolachino <fmolachino@gmail.com>
2023-03-22 19:29:10 -0300

    getting use to it, added a few labels and textBoxes

--- src/interfaze/MainForm.java
+++ src/interfaze/MainForm.java
@@ -3,10 +3,16 @@ package interfaze;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class MainForm {
 
 	private JFrame frame;
+	private JTextField txtInputYourName;
 
 	/**
 	 * Launch the application.
@@ -38,6 +44,24 @@ public class MainForm {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Welcome!");
+		lblNewLabel.setBounds(187, 12, 70, 15);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JButton btnInputYourName = new JButton("Do Stuff");
+		btnInputYourName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnInputYourName.setBounds(137, 77, 185, 25);
+		frame.getContentPane().add(btnInputYourName);
+		
+		txtInputYourName = new JTextField();
+		txtInputYourName.setText("Input your name");
+		txtInputYourName.setBounds(173, 39, 114, 19);
+		frame.getContentPane().add(txtInputYourName);
+		txtInputYourName.setColumns(10);
 	}
-
 }
